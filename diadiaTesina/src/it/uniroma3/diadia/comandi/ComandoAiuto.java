eee10e96709195488b10e70b55ec1ad06fe02367
James Ravago <ravagojames@gmail.com>
2020-08-11 13:10:06 +0200

    Piccole Aggiunte

--- diadiaTesina/src/it/uniroma3/diadia/comandi/ComandoAiuto.java
+++ diadiaTesina/src/it/uniroma3/diadia/comandi/ComandoAiuto.java
@@ -5,14 +5,18 @@ import it.uniroma3.diadia.Partita;
 public class ComandoAiuto extends AbstractComando {
 
 	private final static String NOME = "aiuto";
-	
+
 	static final private String[] ELENCO_COMANDI = {"vai", "aiuto", "fine", "prendi", "posa", "guarda",
 			"interagisci", "saluta", "nonValido"};
-	
+
 	public ComandoAiuto(String[] elencoComandi) {
 		super.setNome(NOME);
 	}
-	
+
+	public ComandoAiuto() {
+
+	}
+
 	@Override
 	public void esegui(Partita partita) {
 		for(int i=0; i< ELENCO_COMANDI.length; i++) 
