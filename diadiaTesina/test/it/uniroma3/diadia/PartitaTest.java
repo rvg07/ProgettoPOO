b27794866e2e16f9603c81fcab78f8006a3fa992
James Ravago <ravagojames@gmail.com>
2020-08-15 22:02:33 +0200

    Partita Test Lombok

--- diadiaTesina/test/it/uniroma3/diadia/PartitaTest.java
+++ diadiaTesina/test/it/uniroma3/diadia/PartitaTest.java
@@ -12,59 +12,65 @@ import it.uniroma3.diadia.ambienti.Stanza;
 
 
 public class PartitaTest {
-	
+
 	private Partita partita;
 	private Labirinto labirinto;
-	
+
 	@Before
 	public void setUp() {
+
+		Stanza stanzaVincente = new Stanza("vincente");
+		Stanza stanzaIniziale = new Stanza("iniziale");
+
+
 		this.labirinto = Labirinto.builder()
-				.addStanzaIniziale("iniziale")
-				.addStanzaVincente("vincente")
+				.addStanzaIniziale(stanzaIniziale)
+				.addStanzaVincente(stanzaVincente)
 				.build();
+
 		this.partita = new Partita(this.labirinto);
 	}
-	
+
 	@Test
 	public void testGetStanzaVincenteNotNull() {
 		assertNotNull(this.partita.getStanzaVincente());
 	}
-	
+
 	@Test
 	public void testVintaSeStanzaCorrenteEVincente() {
 		this.partita.setStanzaCorrente(this.partita.getStanzaVincente());
 		assertTrue(this.partita.vinta());
 	}
-	
+
 	@Test
 	public void testNonVintaSeStanzaCorrenteNonEVincente() {
 		this.partita.setStanzaCorrente(new Stanza("NonVincente"));
 		assertFalse(this.partita.vinta());
 	}
-	
+
 	@Test
 	public void testNonVintaInizioPartita() {
 		assertFalse(this.partita.vinta());
 	}
-	
+
 	@Test
 	public void testFinitaSeVinta() {
 		this.partita.setStanzaCorrente(this.partita.getStanzaVincente());
 		assertTrue(this.partita.isFinita());
 	}
-	
+
 	@Test
 	public void testFinitaSeEsplicitamenteSettato() {
 		this.partita.setFinita();
 		assertTrue(this.partita.isFinita());
 	}
-	
+
 	@Test
 	public void testFinitaSeCFUFiniti() {
 		this.partita.setCfu(0);
 		assertTrue(this.partita.isFinita());
 	}
-	
+
 	@Test
 	public void testNonFinitaInizioPartita() {
 		assertFalse(this.partita.isFinita());
